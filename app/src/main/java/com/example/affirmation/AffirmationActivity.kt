690533c1bf451f2f471db7a23e8628c937f7f76f
balabeltmimi <thanaporn.jira@odds.team>
2021-01-20 19:35:39 +0700

    Add data and change onCreate activity coding style

--- app/src/main/java/com/example/affirmation/AffirmationActivity.kt
+++ app/src/main/java/com/example/affirmation/AffirmationActivity.kt
@@ -2,6 +2,8 @@ package com.example.affirmation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmation.data.Datasource
 import com.example.affirmation.databinding.ActivityAffirmationBinding
 
 class AffirmationActivity : AppCompatActivity() {
@@ -10,6 +12,8 @@ class AffirmationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadAffirmations().size.toString()
         setContentView(binding.root)
     }
 }
