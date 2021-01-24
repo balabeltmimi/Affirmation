0706038a0970abf0a8133c54b5fc77513f7bbbfc
balabeltmimi <thanaporn.jira@odds.team>
2021-01-25 01:30:55 +0700

    Add and modify RecyclerView

--- app/src/main/java/com/example/affirmation/AffirmationActivity.kt
+++ app/src/main/java/com/example/affirmation/AffirmationActivity.kt
@@ -3,6 +3,8 @@ package com.example.affirmation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmation.adapter.ItemAdapter
 import com.example.affirmation.data.Datasource
 import com.example.affirmation.databinding.ActivityAffirmationBinding
 
@@ -12,8 +14,13 @@ class AffirmationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val textView: TextView = findViewById(R.id.textview)
-        textView.text = Datasource().loadAffirmations().size.toString()
+
         setContentView(binding.root)
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
